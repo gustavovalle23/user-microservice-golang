2b6335dcf493740a63b13bfa3841063f5bc30e59
Gustavo Valle <gustavo.vallerp@hotmail.com>
2023-04-02 02:35:39 -0300

    test: moving mocks to appropriate place

---
+++ tests/mocks/database.go
@@ -0,0 +1,43 @@
+package mocks
+
+import (
+	"time"
+
+	"github.com/gustavovalle23/user-microservice-golang/pkg/user/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockUserRepository struct {
+	mock.Mock
+}
+
+func (m *MockUserRepository) Save(user *domain.User) error {
+	args := m.Called(user)
+	return args.Error(0)
+}
+
+func (m *MockUserRepository) FindByDocumentNo(documentNo string) (*domain.User, error) {
+	args := m.Called(documentNo)
+	user, _ := args.Get(0).(*domain.User)
+	return user, args.Error(1)
+}
+
+func (m *MockUserRepository) FindByEmail(email string) (*domain.User, error) {
+	args := m.Called(email)
+	user, _ := args.Get(0).(*domain.User)
+	return user, args.Error(1)
+}
+
+func (m *MockUserRepository) Update(user *domain.User) error {
+	args := m.Called(user)
+	return args.Error(0)
+}
+
+type MockTokenGenerator struct {
+	mock.Mock
+}
+
+func (m *MockTokenGenerator) GenerateToken(userID string, expirationTime time.Time) (string, error) {
+	args := m.Called(userID, expirationTime)
+	return args.String(0), args.Error(1)
+}
