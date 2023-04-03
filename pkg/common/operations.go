f89dedce5709c15309ca5ef2032ce659a645f151
Gustavo Valle <gustavo.vallerp@hotmail.com>
2023-04-03 17:57:47 -0300

    feat: add filter method

--- pkg/common/operations.go
+++ pkg/common/operations.go
@@ -1,17 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func Map[T, U any](fn func(T) U, slice []T) []U {
-	result := make([]U, len(slice))
-	for i, v := range slice {
-		result[i] = fn(v)
+func Map[T, U any](mappingFunc func(T) U, inputSlice []T) []U {
+	outputSlice := make([]U, len(inputSlice))
+
+	for i, elem := range inputSlice {
+		outputSlice[i] = mappingFunc(elem)
 	}
-	return result
+
+	return outputSlice
+}
+
+func Filter[T any](data []T, filterFunc func(T) bool) []T {
+	filteredData := make([]T, 0, len(data))
+
+	for _, elem := range data {
+		if filterFunc(elem) {
+			filteredData = append(filteredData, elem)
+		}
+	}
+
+	return filteredData
 }
 
 func main() {
 	input := []int{1, 2, 3, 4}
 	result := Map(func(x int) int { return x * 2 }, input)
 	fmt.Println(result)
+
+	words := []string{"war", "cup", "water", "tree", "storm"}
+
+	res := Filter(words, func(s string) bool {
+		return strings.HasPrefix(s, "w")
+	})
+
+	fmt.Println(res)
 }
